df100e8fc67de11fcb0f23969162a29f13dbb886
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-05-30 19:56:57 +0300

    add angular app dependency on app

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngResource', 'ngOpenFB'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngResource', 'ngOpenFB', 'braintree-angular'])
 
 .run(function($ionicPlatform, $state, ngFB) {
   $ionicPlatform.ready(function() {
